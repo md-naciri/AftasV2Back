00763f04a75f6a55222151ba22cdf73a8d3bf50b
md_naciri <med.ar.naciri@gmail.com>
2023-12-09 22:00:17 +0100

    add all necessary repositories

---
+++ src/main/java/com/app/fishcompetition/repositories/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.app.fishcompetition.repositories;
+
+import com.app.fishcompetition.model.entity.Competition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Competition, UUID> {
+}
