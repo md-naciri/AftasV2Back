0de56b9ed0cace48f94fe2ab01b64eb8f90626ae
md_naciri <med.ar.naciri@gmail.com>
2023-12-09 16:39:57 +0100

    intialize a spring boot project

---
+++ src/main/java/com/app/fishcompetition/FishCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.app.fishcompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishCompetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/fishcompetition/FishCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fishcompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishCompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
