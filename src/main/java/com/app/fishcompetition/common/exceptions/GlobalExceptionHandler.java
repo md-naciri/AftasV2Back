6341e5be512433f0360c3447b789102d21f9c255
md_naciri <med.ar.naciri@gmail.com>
2023-12-10 13:50:44 +0100

    create method for DataIntegrityViolationException

--- src/main/java/com/app/fishcompetition/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/app/fishcompetition/common/exceptions/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package com.app.fishcompetition.common.exceptions;
 import com.app.fishcompetition.common.responses.RequestResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -30,5 +32,29 @@ public class GlobalExceptionHandler {
                 );
         requestResponse.setDetails(errors);
         return ResponseEntity.badRequest().body(requestResponse);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RequestResponse> handleDataIntegrityViolationException(DataIntegrityViolationException dataIntegrityViolationException) {
+        requestResponse.setTimestamp(LocalDateTime.now());
+        requestResponse.setMessage("Data integrity violation");
+        requestResponse.setStatus("409");
+        Map<String,Object> errors = new HashMap<>();
+
+        String errorMessage = dataIntegrityViolationException.getMessage();
+        if (errorMessage.contains("duplicate key")) {
+            String duplicateField = extractDuplicateField(errorMessage);
+            errors.put("error", "Duplicate " + duplicateField + " found.");
+        } else {
+            errors.put("Error", errorMessage);
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(requestResponse);
+    }
+
+    private String extractDuplicateField(String errorMessage) {
+        String[] split = errorMessage.split(" ");
+        String duplicateField = split[split.length - 1];
+        duplicateField = duplicateField.substring(1, duplicateField.length() - 1);
+        return duplicateField;
     }
 }
