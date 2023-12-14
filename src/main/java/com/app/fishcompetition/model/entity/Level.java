1a0212f1f980d12ecc6c5328bf7ebb12adc111e1
md_naciri <med.ar.naciri@gmail.com>
2023-12-14 09:34:49 +0100

    add scope properties in jsonIdentity

--- src/main/java/com/app/fishcompetition/model/entity/Level.java
+++ src/main/java/com/app/fishcompetition/model/entity/Level.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id",scope = Level.class)
 public class Level {
     @Id
     @GeneratedValue(generator = "uuid2")
